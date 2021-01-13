0f9745ffe2fdaaeae6da7eb8b6375e43d94b7763
MaxKonovalov38 <max.kon.38r@gmail.com>
2021-01-13 21:34:06 +0800

    create func's, create variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+fn get_version() -> u16 {
+	1000
+}
+
+fn usage() {
+	let the_version: u16;
+	the_version = get_version();
+	println!("tinymd, a markdown compiler written by Max");
+	println!("Version {}", the_version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
