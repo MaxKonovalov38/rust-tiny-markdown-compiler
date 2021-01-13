99adf30a1c3bc81948134361023c3d4a60340da3
MaxKonovalov38 <max.kon.38r@gmail.com>
2021-01-13 22:41:21 +0800

    create str var

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
+fn get_title() -> String {
+	// Возвращает строку - 'короткий баннер'
+	let mut the_title = String::from(env!("CARGO_PKG_NAME"));
+	the_title.push_str(" (v");
+	the_title.push_str(env!("CARGO_PKG_VERSION"));
+	the_title.push_str("), ");
+	the_title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+	return the_title;
+}
 
-fn get_version() -> u16 {
-	1000
+fn parse_markdown_file() {
+	// Будет вызываться, когда нам передадут md-файл через cl
+}
+
+fn print_short_banner() {
+	// Выведет название, версию и описание
+	println!("{}", get_title());
+}
+
+fn print_long_banner() {
+	// Выведет короткий баннер плюс пример
+	print_short_banner();
+	let mut line = String::from("Written by: ");
+	line.push_str(env!("CARGO_PKG_AUTHORS"));
+	line.push_str("\nHomepage: ");
+	line.push_str(env!("CARGO_PKG_HOMEPAGE"));
+	line.push_str("\nUsage: tinymd <somefile>.md");
+	println!("{}", line);
 }
 
 fn usage() {
-	let the_version: u16;
-	the_version = get_version();
-	println!("tinymd, a markdown compiler written by Max");
-	println!("Version {}", the_version);
+	print_long_banner();
 }
 
 fn main() {
-    usage();
+	usage();
 }
